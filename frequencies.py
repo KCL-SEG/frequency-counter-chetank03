8b8c2c21a4b67cf1c2e98d690255a667541191d1
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-01 16:52:47 +0100

    add function to convert list to dictionary

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for i in items:
+        if str(i) in frequencies: 
+            frequencies[str(i)] = frequencies[str(i)] + 1
+        else:
+            frequencies[str(i)]=1;
     return frequencies
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
